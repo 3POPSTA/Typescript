cd2f5b2b20355111d651f009a89707e57a64f132
ACLL <120932801+3POPSTA@users.noreply.github.com>
2026-07-28 14:48:35 +0100

    Typescript Types

--- src/test.ts
+++
@@ -1,5 +0,0 @@
-function greet(name: string): string{
-    return `haha, ${name}!`;
-}
-const message: string = greet("World");
-console.log(message);
---
+++ src/types.ts
@@ -0,0 +1,20 @@
+//Typescript Primitives
+
+//Boolean
+let isActive: boolean = true;
+let hasMarried = true;
+let hasNotMarried: boolean = false;
+
+
+//Number
+let decimal: number = 6;
+let hex: number = 0xff00d;
+let binary: number = 0b10101;
+let octal: number = 0o744;
+let float: number = 3.15;
+
+
+//Strings
+let color: string = "blue";
+let fullName: string = "John Doe";
+
