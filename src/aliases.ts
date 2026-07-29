f81b289553155f24e4e5bfee7aec9dff047412b5
ACLL <120932801+3POPSTA@users.noreply.github.com>
2026-07-29 11:45:55 +0100

    Typescript Aliases

---
+++ src/aliases.ts
@@ -0,0 +1,23 @@
+//Aliases
+type CarYear = number;
+type CarType = string;
+type CarModel = string;
+
+type Car = {
+    year: CarYear,
+    type: CarType,
+    model: CarModel,
+}
+
+const carYear: CarYear = 2001;
+const carType: CarType = "Toyota";
+const carModel: CarModel = "Corolla";
+
+const car: Car = {
+    year:carYear,
+    type: carType,
+    model: carModel
+}
+console.log(car.year)
+console.log(car.type)
+console.log(car.model)
