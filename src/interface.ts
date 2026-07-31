01ee3c67cba27b3f34d2f1264a8aa080253c2602
ACLL <120932801+3POPSTA@users.noreply.github.com>
2026-07-31 13:57:42 +0100

    Typescript Interface

---
+++ src/interface.ts
@@ -0,0 +1,42 @@
+//interface
+//interface keyword is used to define the shape of an object.
+
+interface User {
+    name:string;
+    age:number;
+}
+
+const user: User = {
+    name: "ACLL",
+    age:12345,
+}
+
+//Extending an interface
+interface Person {
+    name:string;
+}
+
+interface Employee extends Person{
+    salary:number;
+}
+const emp:Employee = {
+    name:"ACLL",
+    salary:1234567890
+}
+
+//interface of functions
+interface Add {
+    (a:number,b:number) :number;
+}
+const add: Add = (a,b) => a + b;
+
+//interface
+interface Animal{
+    makeSound():void;
+}
+
+class Dog implements Animal{
+    makeSound(): void {
+        console.log("woof!")
+    }
+}
