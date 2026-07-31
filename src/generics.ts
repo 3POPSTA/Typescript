f7e904bdfa6599e1f6b35fbdd2d63860ecf9de1e
ACLL <120932801+3POPSTA@users.noreply.github.com>
2026-07-31 10:10:37 +0100

    Typescript Generics 1

--- src/generics.ts
+++ src/generics.ts
@@ -9,3 +9,41 @@ console.log(createPair<string,number>("hello",23));
 
 
 //Generic Classes
+class NameValue<T>{
+    private _value: T | undefined;
+
+    constructor(private name: string){}
+
+    public setValue(value:T){
+        this._value = value;
+    }
+
+    public getValue(): T | undefined{
+        return this._value;
+    }
+    public toSting(): string{
+        return `${this.name}: ${this._value}`;
+    }
+}
+let value = new NameValue<number>("myNumber");
+value.setValue(10);
+console.log(value.toString());
+
+
+//simple Generics
+function getFirstElement<ElementType>(array: ElementType[]){
+    return array[0];
+}
+const numbers = [1,2,3];
+const firstNum = getFirstElement<number>(numbers)
+
+const strings = ["name","age"];
+const firstStr = getFirstElement<string>(strings);
+
+
+//type Aliases
+type Wrapped<Type> = {value:Type};
+const wrappedValue: Wrapped<number> = {value:10};
+const wrappedValue2 : Wrapped<string> = {value:"1234567890"};
+ console.log(wrappedValue);
+ console.log(wrappedValue2);
